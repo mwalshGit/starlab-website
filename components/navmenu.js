636f6fd1bcb7e5bc5298ff74a085480f2b6b549a
mwalshGit <mwalsh@tamu.edu>
2026-07-27 12:17:47 -0500

    Redesign Starlab homepage: update components/navmenu.js

--- components/navmenu.js
+++ components/navmenu.js
@@ -1,42 +1,24 @@
-
 import Link from 'next/link'
-import { getPageMetadata } from "../utils/pageData"
 
+const NavMenu = () => {
+  const pages = [
+    { link: '/#capabilities', text: 'Capabilities' },
+    { link: '/#facility', text: 'Facility' },
+    { link: '/#research', text: 'Research & Work' },
+    { link: '/pages/4_Student_Work', text: 'Student Work' },
+    { link: '/#collaborate', text: 'Collaborate' },
+  ]
 
-const NavMenu = (props) => {
-
-    const searchParams = props.searchParams
-    const  pid  = searchParams
-
-
-    const pages = getPageMetadata('markdown')
-
-    const pageNames = pages.map(page => {
-        return {
-            link: page.slug,
-            text: page.slug.replace(/[0-9_]/g, ' ')
-        }
-    })
-
-    return (
-        <nav>
-            <ul>
-                <li key='starlab'>
-                    <Link href='/' alt='Starlab Home Page'>Starlab</Link>
-                </li>
-                {pageNames.map(page => (
-                    <li key={page.text} className={pid == page.text ? 'hilight' : ''}>
-                        <Link href={`/pages/${page.link}`}>{page.text}</Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    )
-}
-
-export async function getStaticProps({ params }) {
-    const pages = getPages()
-    return {pages, fallback: false}
+  return (
+    <nav>
+      <ul>
+        <li><Link href='/' aria-label='Starlab home'>Starlab</Link></li>
+        {pages.map((page) => (
+          <li key={page.text}><Link href={page.link}>{page.text}</Link></li>
+        ))}
+      </ul>
+    </nav>
+  )
 }
 
 export default NavMenu
